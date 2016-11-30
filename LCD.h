c7b1fd456a9fc6b84561f5fc9e6d8f1e07755bb8
Aaron Ewing <aaronewing@nnu.edu>
2016-11-29 20:45:24 -0700

    update on LCD

---
+++ LCD.h
@@ -0,0 +1,16 @@
+/*
+ * LCD.h
+ *
+ *  Created on: Nov 29, 2016
+ *      Author: AaronEwing & MAAHDIIIY!!!!! XD ^.^ :)
+ */
+
+#ifndef LCD_H_
+#define LCD_H_
+
+void Init_LCD(void);
+void displayScrollText(char*);
+void showChar(char, int);
+void clearLCD(void);
+
+#endif /* LCD_H_ */
